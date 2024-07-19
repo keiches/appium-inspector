c796ad5b3f1a4f283c95affa621e2ba660ebabbc
김한수 <keiches@sptek.co.kr>
2024-07-19 11:28:38 +0900

    🐛 fix: TS9006 error

--- app/common/renderer/actions/index.js
+++ app/common/renderer/actions/index.js
@@ -1,7 +1,9 @@
-// NOTE: tsconfig.json에 "allowJs: false"를 하면 해결되나, runtime 때, process 오류를 발생
 import * as inspectorActions from './Inspector';
 import * as sessionActions from './Session';
 
+/**
+ * @type any
+ */
 export default {
   ...inspectorActions,
   ...sessionActions,
