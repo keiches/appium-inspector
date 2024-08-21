9c7ed5b2eba88eb0d8bf8a45d8604bc018312c06
김한수 (MBP2) <keiches@gmail.com>
2024-08-21 18:19:23 +0900

    feat: separate requirements about working space and node

--- app/electron/main/main.js
+++ app/electron/main/main.js
@@ -1,5 +1,6 @@
-import {app, dialog} from 'electron';
+import {app, dialog, remote} from 'electron';
 import debug from 'electron-debug';
+import {existsSync} from 'fs';
 
 // import {installExtensions} from './debug';
 import {getAppiumSessionFilePath, isDev} from './helpers';
@@ -24,6 +25,8 @@ import {getDevices} from './device-manager';
 export let openFilePath = getAppiumSessionFilePath(process.argv, app.isPackaged);
 
 let server;
+let compiler;
+let runner;
 
 app.on('open-file', (event, filePath) => {
   openFilePath = filePath;
@@ -52,121 +55,31 @@ async function spawn(cmd) {
 }
 */
 
-async function runServer0() {
-  /*const controller = new AbortController();
-  const { signal } = controller;
-  server = fork('../server/build/lib/main.js', [], {*/
-  /*server = fork('node', ['../server/build/lib/main'], {
-    // signal,
-    stdio: ['pipe', 'inherit', 'inherit'],
-  });*/
+const checkEnvironments = (process.env.NODE_ENV === 'development') ? () => {
+  log.log(`----App Path> ${app.getAppPath()}`);
+  // NOTE: in renderer/preload, use "remote.app.getAppPath()"
+  log.log(`----Current Directory> ${__dirname}`);
+  log.log(`----HOME> ${homedir()}`);
+  log.log(`----WORKING_HOME> ${resolve(homedir(), '.aav')}`);
+  log.log(`----WORKING_HOME exists> ${existsSync(resolve(homedir(), '.aav'))}`);
+  log.log(`----APPIUM_HOME> ${process.env.APPIUM_HOME}`);
+  log.log(`----APPIUM_HOME> ${existsSync(process.env.APPIUM_HOME)}`);
+  log.log(`----JAVA_HOME> ${process.env.JAVA_HOME}`);
+  log.log(`----JAVA_HOME> ${existsSync(process.env.JAVA_HOME)}`);
+  log.log(`----JDK_HOME> ${process.env.JDK_HOME}`);
+  log.log(`----JDK_HOME> ${existsSync(process.env.JDK_HOME)}`);
+  log.log(`----ANDROID_HOME> ${process.env.ANDROID_HOME}`);
+  log.log(`----ANDROID_HOME> ${existsSync(process.env.ANDROID_HOME)}`);
+} : () => {};
+
+/**
+ * Get Node.Js executable path
+ * @returns {Promise<string>}
+ */
+async function resolveNodePath() {
   // const nodePath = await resolveExecutablePath('node');
   const nodePath = await NodeDetector.detect();
-  if (nodePath) {
-    log.log(`Node is installed at: ${nodePath}. ${
-      (await exec('node', ['--version'])).stdout.split('\n')[0]
-    }`);
-
-    // server = spawn(nodePath, ['../server/build/lib/main.js']/*, {
-    server = spawn(nodePath, ['../node_modules/appium/build/lib/main.js']/*, {
-      stdio: ['pipe', 'inherit', 'inherit'],
-    }*/);
-
-    server.on('exit', (code, signal) => {
-      // if we get here, all we know is that the proc exited
-      log.log(`exited with code ${code} from signal ${signal}`);
-      // exited with code 127 from signal SIGHUP
-    });
-
-    server.on('stop', (code, signal) => {
-      // if we get here, we know that we intentionally stopped the proc
-      // by calling proc.stop
-    });
-
-    server.on('end', (code, signal) => {
-      // if we get here, we know that the process stopped outside of our control
-      // but with a 0 exit code
-    });
-
-    server.on('die', (code, signal) => {
-      // if we get here, we know that the process stopped outside of our control
-      // with a non-zero exit code
-    });
-
-    server.on('output', (stdout, stderr) => {
-      log.log(`stdout: ${stdout}`);
-      log.log(`stderr: ${stderr}`);
-    });
-
-    // lines-stderr is just the same
-    server.on('lines-stdout', (lines) => {
-      log.log(lines);
-      // ['foo', 'bar', 'baz']
-      // automatically handles rejoining lines across stream chunks
-    });
-
-    // stream-line gives you one line at a time, with [STDOUT] or [STDERR]
-    // prepended
-    server.on('stream-line', (line) => {
-      log.log(line);
-      // [STDOUT] foo
-    });
-
-    await server.start();
-    log.log(`server spawned: ${server.isRunning}`);
-
-    /*
-    let input = '';
-    process.stdin.on('data', (chunk) => {
-      input += chunk;
-    });
-
-    process.stdin.on('end', (chunk) => {
-      eval(input);
-    });
-
-    process.stdin.on('error', (err) => {
-      log.error(`process::stdin:\n${err}`);
-    });
-    */
-
-    /*server.stdout.on('data', (data) => {
-      log.log(`appium-server stdout:\n${data}`);
-    });
-
-    server.stderr.on('data', (data) => {
-      log.error(`appium-server stderr:\n${data}`);
-    });
-
-    server.on('message', (message) => {
-      log.log('[appium-server] ' + message);
-    });
-
-    server.on('error', (err) => {
-      // This will be called with err being an AbortError if the controller aborts
-      log.error('[appium-server] ' + err.toString());
-      /!*dialog.showMessageBox({
-        type: 'error',
-        buttons: [t('OK')],
-        message: err.message,
-      });*!/
-    });
-
-    server.on('close', (code) => {
-      // app.quit();
-      log.log('[appium-server] close on code: ' + code);
-    });
-
-    server.on('exit', (code) => {
-      log.log('[appium-server] exit on code: ' + code);
-    });
-    */
-
-    app.on('window-all-closed', async () => {
-      // controller.abort();
-      await server.stop();
-    });
-  } else {
+  if (!nodePath) {
     log.error('node cannot be found');
     await dialog.showMessageBox({
       type: 'error',
@@ -174,9 +87,20 @@ async function runServer0() {
       message: 'node cannot be found',
     });
   }
+
+  log.log(`Node is installed at: ${nodePath}. ${
+    (await exec('node', ['--version'])).stdout.split('\n')[0]
+  }`);
+
+  return nodePath;
 }
 
-async function runServer() {
+/**
+ * Execute Appium server in background
+ * @param {string} nodePath
+ * @returns {Promise<void>}
+ */
+async function runAppiumServer(nodePath) {
   /*const controller = new AbortController();
   const { signal } = controller;
   server = fork('../server/build/lib/main.js', [], {*/
@@ -184,109 +108,168 @@ async function runServer() {
     // signal,
     stdio: ['pipe', 'inherit', 'inherit'],
   });*/
-  // const nodePath = await resolveExecutablePath('node');
-  const nodePath = await NodeDetector.detect();
-  if (nodePath) {
-    log.log(`Node is installed at: ${nodePath}. ${
-      (await exec('node', ['--version'])).stdout.split('\n')[0]
-    }`);
+  // server = spawn(nodePath, [join(__dirname, '../server/build/lib/main.js')]/*, {
+  // server = spawn(nodePath, ['../node_modules/appium/build/lib/main.js']/*, {
+  server = spawn(nodePath, [
+    // TODO: search appium server
+    // join(__dirname, '../node_modules/ap\pium/build/lib/main.js'),
+    // 'C:\\opt\\nodejs\\node_modules\\appium\\index.js',
+    join('C:\\Users\\keiches\\Projects\\open\\appium\\appium\\packages\\appium\\build\\lib\\main.js'),
+    // resolve(join('..', 'server', 'packages', 'appium')),
+    // ../server/packages/appium
+    'server',
+    // '--show-config'
+    '--config',
+    join(__dirname, '../configs/server.conf.js'),
+  ], {
+    // stdio: ['pipe', 'inherit', 'inherit']
+    // shell: true,
+    // cwd: 'C:\\Test\\Path',
+    // detached: true, ==> server.unref();
+    // NOTE: 아래 값을 넣으면, 더 이상 shell 로 부터 환경설정값을 읽지 않는 듯함
+    env: {
+      // 'APPIUM_HOME': join(__dirname, '.appium'),
+      'APPIUM_HOME': resolve(homedir(), '.aav'),
+      'ANDROID_HOME': 'C:\\opt\\Android\\Sdk',
+    },
+  });
 
-    log.log(`----1> ${__dirname}`);
-    log.log(`----2> ${homedir()}`);
-    log.log(`----3> ${resolve(homedir(), '.aav')}`);
+  server.stdout.on('data', (data) => {
+    // if we get here, all we know is that the proc exited
+    log.log(`[appium-server] stdout: ${data}`);
+    // exited with code 127 from signal SIGHUP
+  });
 
-    // server = spawn(nodePath, [join(__dirname, '../server/build/lib/main.js')]/*, {
-    // server = spawn(nodePath, ['../node_modules/appium/build/lib/main.js']/*, {
-    server = spawn(nodePath, [
-      // TODO: search appium server
-      // join(__dirname, '../node_modules/ap\pium/build/lib/main.js'),
-      // 'C:\\opt\\nodejs\\node_modules\\appium\\index.js',
-      'C:\\Users\\keiches\\Projects\\open\\appium\\appium\\packages\\appium\\build\\lib\\main.js',
-      'server',
-      '--config',
-      join(__dirname, '../configs/server.conf.js'),
-      // '--show-config'
-    ], {
-      // stdio: ['pipe', 'inherit', 'inherit']
-      // shell: true,
-      // cwd: 'C:\\Test\\Path',
-      // detached: true, ==> server.unref();
-      // NOTE: 아래 값을 넣으면, 더 이상 shell 로 부터 환경설정값을 읽지 않는 듯함
-      env: {
-        // 'APPIUM_HOME': join(__dirname, '.appium'),
-        'APPIUM_HOME': resolve(homedir(), '.aav'),
-        'ANDROID_HOME': 'C:\\opt\\Android\\Sdk',
-      },
-    });
+  server.stderr.on('data', (data) => {
+    log.error(`[appium-server] stderr: ${data}`);
+  });
 
-    server.stdout.on('data', (data) => {
-      // if we get here, all we know is that the proc exited
-      log.log(`[appium-server] stdout: ${data}`);
-      // exited with code 127 from signal SIGHUP
-    });
+  server.on('message', (message) => {
+    log.log('[appium-server] message:' + message);
+  });
 
-    server.stderr.on('data', (data) => {
-      log.error(`[appium-server] stderr: ${data}`);
-    });
-
-    server.on('message', (message) => {
-      log.log('[appium-server] message:' + message);
-    });
-
-    server.on('error', (err) => {
-      // This will be called with err being an AbortError if the controller aborts
-      log.error('[appium-server] error:' + err.toString());
-      dialog.showMessageBox({
-        type: 'error',
-          buttons: ['OK'],
-          message: err.message,
-      });
-    });
-
-    server.on('disconnect', () => {
-      log.warn('[appium-server] disconnect');
-    });
-
-    server.on('close', (code, signal) => {
-      // if we get here, we know that the process stopped outside our control
-      // but with a 0 exit code
-      // app.quit();
-      log.log(`[appium-server] closed with code ${code} from signal ${signal}`);
-    });
-
-    server.on('exit', (code) => {
-      log.log('[appium-server] exit on code: ' + code);
-    });
-
-    log.log(`[appium-server] spawned: ${server.pid}`);
-
-    app.on('window-all-closed', () => {
-      // controller.abort();
-      if (server && !server.killed) {
-        server.kill('SIGTERM'); // NodeJS.Signals
-        server = null;
-      }
-    });
-  } else {
-    log.error('node cannot be found');
-    await dialog.showMessageBox({
+  server.on('error', (err) => {
+    // This will be called with err being an AbortError if the controller aborts
+    log.error('[appium-server] error:' + err.toString());
+    dialog.showMessageBox({
       type: 'error',
       buttons: ['OK'],
-      message: 'node cannot be found',
+      message: err.message,
     });
-  }
+  });
+
+  server.on('disconnect', () => {
+    log.warn('[appium-server] disconnect');
+  });
+
+  server.on('close', (code, signal) => {
+    // if we get here, we know that the process stopped outside our control
+    // but with a 0 exit code
+    // app.quit();
+    log.log(`[appium-server] closed with code ${code} from signal ${signal}`);
+  });
+
+  server.on('exit', (code) => {
+    log.log('[appium-server] exit on code: ' + code);
+  });
+
+  log.log(`[appium-server] spawned: ${server.pid}`);
+
+  app.on('window-all-closed', () => {
+    // controller.abort();
+    if (server && !server.killed) {
+      server.kill('SIGTERM'); // NodeJS.Signals
+      server = null;
+    }
+  });
 }
 
+/**
+ * Execute actions handler in background
+ * @param {string} testId
+ * @returns {Promise<void>}
+ */
+async function runActionsTester(testId) {
+  // #1 compile java to class
+  compiler = spawn(join(__dirname, '..', 'libs', 'actions-tester', 'compile'), [
+    testId,
+  ], {
+    stdio: ['pipe', 'inherit', 'inherit']
+    // shell: true,
+    // cwd: 'C:\\Test\\Path',
+    // detached: true, ==> server.unref();
+  });
+
+  server.stdout.on('data', (data) => {
+    // if we get here, all we know is that the proc exited
+    log.log(`[appium-actions-tester] stdout: ${data}`);
+    // exited with code 127 from signal SIGHUP
+  });
+
+  server.stderr.on('data', (data) => {
+    log.error(`[appium-actions-tester] stderr: ${data}`);
+  });
+
+  server.on('message', (message) => {
+    log.log('[appium-actions-tester] message:' + message);
+  });
+
+  server.on('error', (err) => {
+    // This will be called with err being an AbortError if the controller aborts
+    log.error('[appium-actions-tester] error:' + err.toString());
+    dialog.showMessageBox({
+      type: 'error',
+      buttons: ['OK'],
+      message: err.message,
+    });
+  });
+
+  server.on('disconnect', () => {
+    log.warn('[appium-actions-tester] disconnect');
+  });
+
+  server.on('close', (code, signal) => {
+    // if we get here, we know that the process stopped outside our control
+    // but with a 0 exit code
+    // app.quit();
+    log.log(`[appium-actions-tester] closed with code ${code} from signal ${signal}`);
+  });
+
+  server.on('exit', (code) => {
+    log.log('[appium-actions-tester] exit on code: ' + code);
+  });
+
+  log.log(`[appium-actions-tester] spawned: ${server.pid}`);
+
+  app.on('window-all-closed', () => {
+    // controller.abort();
+    if (server && !server.killed) {
+      server.kill('SIGTERM'); // NodeJS.Signals
+      server = null;
+    }
+  });
+}
+
+// launchinfo for Windows, Linux
+// app.on('will-finish-launch', () => {});
+// launchinfo for macOS
 app.on('ready', async () => {
   if (isDev) {
+    checkEnvironments();
     debug();
     // TODO: uncomment this after upgrading to Electron 15+
     // await installExtensions();
   }
 
-  // @site: https://www.freecodecamp.org/korean/news/node-js-child-processes-everything-you-need-to-know-e69498fe970a/
-  // TODO: "spawn({detached})"로 호출할 지 확인 후 결정
-  // await runServer();
+  const nodePath = await resolveNodePath();
+  if (nodePath) {
+    // @site: https://www.freecodecamp.org/korean/news/node-js-child-processes-everything-you-need-to-know-e69498fe970a/
+    // TODO: "spawn({detached})"로 호출할 지 확인 후 결정
+    // await runAppiumServer(nodePath);
+
+    // TODO: "spawn({detached})"로 호출할 지 확인 후 결정
+    // await runActionsTester(nodePath);
+  }
 
   await getDevices();
 
--- app/electron/preload/preload.js
+++ app/electron/preload/preload.js
@@ -1,1 +1,26 @@
 // Required by Vite
+// for debug
+/*
+window.addEventListener('DOMContentLoaded', () => {
+  document.body.append <= document.createElement(`<div style="position: absolute; top: 100px; z-index: 1000;">
+      <p>
+        Process <span id="process-execpath"></span>,
+        Node.js <span id="node-version"></span>,
+        Chromium <span id="chrome-version"></span>,
+        Electron <span id="electron-version"></span>.
+      </p>
+    </div>`)
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) {
+      element.innerText = text;
+    }
+  };
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type]);
+  }
+
+  document.getElementById('process-execpath').innerText = process.execPath;
+});
+*/
